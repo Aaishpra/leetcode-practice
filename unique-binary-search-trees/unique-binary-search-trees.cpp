cc7635d5a9ebb1dd4348b003fed670abf35fb3ff
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2021-11-09 01:21:46 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (88.05%) - LeetHub

---
+++ unique-binary-search-trees/unique-binary-search-trees.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    //Memoization solution
+    int dp[20]{};
+    int numTrees(int n) {
+        if(n<=1) return 1;
+        if(dp[n]) return dp[n];
+        for(int i=1;i<=n;i++){
+            dp[n]+=numTrees(i-1)*numTrees(n-i);
+        }
+        return dp[n];
+    }
+};
