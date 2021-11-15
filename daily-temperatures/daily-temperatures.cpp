8b937f1bf8a2f01a766004e9236731bec7a933d3
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2021-11-15 08:39:21 +0530

    Time: 180 ms (25.19%), Space: 90 MB (14.06%) - LeetHub

--- daily-temperatures/daily-temperatures.cpp
+++ daily-temperatures/daily-temperatures.cpp
@@ -1,17 +1,59 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-     int n = temperatures.size();
-stack<int> s;
-vector<int> res;
-for(int i=n-1; i>=0; i--) {
-while(!s.empty() && temperatures[s.top()] <= temperatures[i]) {
-s.pop();
-}
-res.push_back((s.size() == 0) ? 0 : s.top()-i);
-s.push(i);
-}
-reverse(res.begin(),res.end());
-return res;
+        stack<pair<int,int>> s;
+        vector<int> v;
+        for(int i=temperatures.size()-1;i>=0;i--){
+            if(s.size()==0){v.push_back(0);}
+            if(s.size()!=0 && s.top().first>temperatures[i])
+            {v.push_back(s.top().second-i);}
+            else if(s.size()!=0 && s.top().first<=temperatures[i]){
+                while(s.size()!=0 && s.top().first<=temperatures[i]){
+                    s.pop();
+                }
+                if(s.size()==0){v.push_back(0);}
+                else {v.push_back(s.top().second-i);}
+            }
+            s.push({temperatures[i],i});
+        }
+       
+        reverse(v.begin(),v.end());
+         return v;
     }
+// vector<int> dailyTemperatures(vector<int>& T)
+// {
+// stack<pair<int,int>>st;
+// vector<int> v;
+// int i;
+// for( i=T.size()-1;i>=0;i--)
+// {
+// if(st.size()==0)
+// {
+// v.push_back(0);
+// }
+// else if(st.size()!=0 && st.top().first>T[i])
+// {
+// v.push_back(st.top().second-i);
+// }
+// else if(st.size()!= 0 && st.top().first<=T[i])
+// {
+// while(st.size()!=0 && st.top().first<=T[i])
+// {
+// st.pop();
+// }
+// if(st.size()==0)
+// {
+// v.push_back(0);
+// }
+// else
+// {
+// v.push_back(st.top().second-i);
+// }
+// }
+// st.push({T[i],i});
+// }
+
+//     reverse(v.begin(),v.end());
+//     return v;
+// }
 };
