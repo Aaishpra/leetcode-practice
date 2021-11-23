0e28e8c4be5c1f3485a6bec5c19b79a2d246f170
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2021-11-23 15:26:27 +0530

    Time: 20 ms (14.63%), Space: 9.9 MB (77.65%) - LeetHub

---
+++ surrounded-regions/surrounded-regions.cpp
@@ -0,0 +1,46 @@
+class Solution {
+public:
+    void dfs(vector<vector<char>>& grid, int i, int j){
+        if(i<0 || j<0 || i>=grid.size() || j>=grid[0].size() ||grid[i][j]!='O') return;
+        grid[i][j]='#';
+        dfs(grid,i+1,j);
+        dfs(grid,i,j+1);
+        dfs(grid,i-1,j);
+        dfs(grid,i,j-1);
+    }
+    void solve(vector<vector<char>>& board) {
+         
+     int m = board.size();
+        
+      if(m == 0) return;  
+        
+     int n = board[0].size();
+     
+     //Moving over firts and last column   
+     for(int i=0; i<m; i++) {
+         if(board[i][0] == 'O')
+             dfs(board, i, 0);
+         if(board[i][n-1] == 'O')
+             dfs(board, i, n-1);
+     }
+        
+        
+     //Moving over first and last row   
+     for(int j=0; j<n; j++) {
+         if(board[0][j] == 'O')
+             dfs(board, 0, j);
+         if(board[m-1][j] == 'O')
+             dfs(board, m-1, j);
+     }
+        
+     for(int i=0; i<m; i++)
+         for(int j=0; j<n; j++)
+         {
+             if(board[i][j] == 'O')
+                 board[i][j] = 'X';
+             if(board[i][j] == '#')
+                 board[i][j] = 'O';
+         }
+    }
+    
+};
