44377bdaa6a7f19ee7fcfc3c88c25da476f9c39f
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2021-12-13 20:44:52 +0530

    Time: 4 ms (99.97%), Space: 10 MB (52.18%) - LeetHub

--- surrounded-regions/surrounded-regions.cpp
+++ surrounded-regions/surrounded-regions.cpp
@@ -16,22 +16,26 @@ public:
         
      int n = board[0].size();
      
-     //Moving over firts and last column   
+     //Moving over first and last column   
      for(int i=0; i<m; i++) {
-         if(board[i][0] == 'O')
-             dfs(board, i, 0);
-         if(board[i][n-1] == 'O')
-             dfs(board, i, n-1);
-     }
+         for (int j = 0; j < n; j++){
+         if (i == 0 || j == 0 || i == m-1 || j == n-1){
+             if(board[i][j]=='O'){
+ 					dfs(board, i, j);}}}}
+//          if(board[i][0] == 'O')
+//              dfs(board, i, 0);
+//          if(board[i][n-1] == 'O')
+//              dfs(board, i, n-1);
+//      }
         
         
-     //Moving over first and last row   
-     for(int j=0; j<n; j++) {
-         if(board[0][j] == 'O')
-             dfs(board, 0, j);
-         if(board[m-1][j] == 'O')
-             dfs(board, m-1, j);
-     }
+//      //Moving over first and last row   
+//      for(int j=0; j<n; j++) {
+//          if(board[0][j] == 'O')
+//              dfs(board, 0, j);
+//          if(board[m-1][j] == 'O')
+//              dfs(board, m-1, j);
+//      }
         
      for(int i=0; i<m; i++)
          for(int j=0; j<n; j++)
