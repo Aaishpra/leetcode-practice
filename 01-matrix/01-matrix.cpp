34c745d5eb6a8374604e282eca6d01785c5793e6
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2021-12-15 09:34:05 +0530

    Time: 52 ms (98.54%), Space: 27.3 MB (98.75%) - LeetHub

--- 01-matrix/01-matrix.cpp
+++ 01-matrix/01-matrix.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
-    vector<int> DIR = {0, 1, 0, -1, 0};
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
-        int m = mat.size(), n = mat[0].size();
-        queue<pair<int, int>> q;
-        for (int r = 0; r < m; ++r)
-            for (int c = 0; c < n; ++c)
-                if (mat[r][c] == 0) q.emplace(r, c);
-                else mat[r][c] = -1; // Marked as not processed yet!
-
-        while (!q.empty()) {
-            auto [r, c] = q.front(); q.pop();
-            for (int i = 0; i < 4; ++i) {
-                int nr = r + DIR[i], nc = c + DIR[i+1];
-                if (nr < 0 || nr == m || nc < 0 || nc == n || mat[nr][nc] != -1) continue;
-                mat[nr][nc] = mat[r][c] + 1;
-                q.emplace(nr, nc);
+        int n=mat.size(), m=mat[0].size(), inf=m+n;
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                 if(mat[i][j]==0) continue;
+                int top=inf, left=inf;
+                    if(i-1>=0) top=mat[i-1][j];
+                    if(j-1>=0) left=mat[i][j-1];
+                mat[i][j]=min(top,left)+1;
+            }
+        }
+         
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                 if(mat[i][j]==0) continue;
+                int bot=inf, right=inf;
+                    if(i+1<=n-1) bot=mat[i+1][j];
+                    if(j+1<=m-1) right=mat[i][j+1];
+                mat[i][j]=min(mat[i][j],min(bot,right)+1);
             }
         }
         return mat;
