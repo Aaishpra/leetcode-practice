65465204831df4f05e7db5635f48e4d91a30213f
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-01-14 21:55:03 +0530

    Time: 12 ms (92.96%), Space: 18.5 MB (89.20%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.cpp
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -4,7 +4,7 @@ public:
          int left=0, len = citations.size(), right= len-1,  mid;
         while(left<=right)
         {
-            mid=(left+right)>>1;
+            mid=left+(right-left)/2;
             if(citations[mid]== (len-mid)) return citations[mid];
             else if(citations[mid] > (len-mid)) right = mid - 1;
             else left = mid + 1;
