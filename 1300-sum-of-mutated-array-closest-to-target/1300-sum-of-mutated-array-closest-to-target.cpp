de5da88c42c7501cbf5945d93e9a63ec9b311e46
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-01-14 22:16:55 +0530

    Time: 32 ms (17.51%), Space: 12.4 MB (34.26%) - LeetHub

---
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int isValid(vector<int>& arr, int target,int mid,int n){
+        int sum=0;
+        
+        for(int i=0;i<n;i++){
+            sum+=min(arr[i],mid);
+        }
+        return abs(target-sum);
+    }
+    int findBestValue(vector<int>& arr, int target) {
+        int n=arr.size();
+        int lo=0;
+        int hi=target;
+        
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(isValid(arr,target,mid,n)<=isValid(arr,target,mid+1,n)) hi=mid;
+            else lo=mid+1;
+        }
+        return lo;
+    }
+};
