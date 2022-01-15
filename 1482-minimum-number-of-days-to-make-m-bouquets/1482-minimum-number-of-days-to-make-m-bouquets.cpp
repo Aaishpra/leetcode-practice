4fc4b0900e9c8e0d3e394c8824df83419b4cd62d
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-01-15 14:12:19 +0530

    Time: 167 ms (64.37%), Space: 63.3 MB (69.29%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.cpp
@@ -1,37 +1,31 @@
 class Solution {
-
-    bool isValid(vector<int>& bloomDay, int m, int k, int mid) {
-
-		int count = 0, size = 0;
-		for (int i = 0; i < bloomDay.size(); i++) {
-			size = (bloomDay[i] <= mid) ? size+1 : 0;
-			if (size == k) size = 0, count++;
-			if (count == m)
-				return true;
-		}
-
-		return false;
-	}
+bool canHarvest(vector<int>& bDay, int harvestDay, int m, int k) {
+    int adjacent = 0;
+    for (auto bloomDay : bDay) {
+        adjacent = bloomDay <= harvestDay ? adjacent + 1 : 0;
+        if (adjacent == k) {
+            adjacent = 0;
+            --m;
+        }
+    }
+    return m <= 0;
+}
 public:
-	int minDays(vector<int>& bloomDay, int m, int k) {
-		if(bloomDay.size() == 0 || m == 0 || k == 0) return 0;
-		if (m * k > bloomDay.size()) return -1;
-
-		int l = INT_MAX, r = INT_MIN;
-		for (int i = 0; i < bloomDay.size(); i++) {
-			l = min(l, bloomDay[i]);
-			r = max(r, bloomDay[i]);
-		}
-
-		while (l <= r) {
-			int mid = l + (r-l)/2;
-
-			if (isValid(bloomDay, m, k, mid))
-				r = mid-1;
-			else
-				l = mid+1;
-		}
-
-		return l;
-	}
+int minDays(vector<int>& bDay, int m, int k) {
+    int mx=INT_MIN;
+    for(int i:bDay){
+        mx=max(i,mx);
+    }
+    if (m * k > bDay.size())
+        return -1;
+    auto lo = 1, hi = mx;
+    while (lo < hi) {
+        auto mid = (hi + lo) / 2;
+        if (canHarvest(bDay, mid, m, k))
+            hi = mid;
+        else
+            lo = mid + 1;
+    }
+    return lo;
+}
 };
