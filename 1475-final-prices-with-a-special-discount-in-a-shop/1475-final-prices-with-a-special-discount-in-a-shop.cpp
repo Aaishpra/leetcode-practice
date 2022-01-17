b2c195976d6002725f07e43b0af1d8fd25b3c1d4
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-01-17 14:20:47 +0530

    Time: 0 ms (100.00%), Space: 10.1 MB (5.79%) - LeetHub

--- 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
     vector<int> finalPrices(vector<int>& prices) {
-        stack<int> minStk;
-        int n = prices.size();
-        vector<int> discount(n, 0);
-        for (int i{n-1}; i>=0; i--) {
-            if (!minStk.empty())
-                if (minStk.top() <= prices[i])
-                    discount[i] = minStk.top();
-                else {
-                    while (!minStk.empty() && (minStk.top() > prices[i])) minStk.pop();
-                    if (!minStk.empty()) discount[i] = minStk.top();
+        //Netx Smaller element  to right
+        stack<int> s;
+        vector<int> res;
+        int n=prices.size();
+        
+        for(int i=n-1;i>=0;i--){
+            if(s.size()==0) res.push_back(prices[i]);
+            else if(s.size()!=0 and s.top()<=prices[i]) res.push_back(prices[i]-s.top());
+            else if(s.size()!=0 and s.top()>=prices[i]){
+                while(s.size()>0 and s.top()>prices[i]){
+                    s.pop();
                 }
-            minStk.push(prices[i]);
+                if(s.size()==0) res.push_back(prices[i]);
+                else res.push_back(prices[i]-s.top());
+            }
+            s.push(prices[i]);
         }
-        vector<int> selling_price(n);
-        for (int i{0}; i<n; i++)
-            selling_price[i] = prices[i] - discount[i];
-        return selling_price;
+        
+        reverse(res.begin(),res.end());
+        return res;
     }
 };
