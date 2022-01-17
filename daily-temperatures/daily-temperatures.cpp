f03d23043ee36c38954c2ec303933f0180dc0932
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-01-17 13:54:17 +0530

    Time: 377 ms, Memory: 90 MB - LeetHub

--- daily-temperatures/daily-temperatures.cpp
+++ daily-temperatures/daily-temperatures.cpp
@@ -1,59 +1,24 @@
 class Solution {
 public:
     vector<int> dailyTemperatures(vector<int>& temperatures) {
-        stack<pair<int,int>> s;
-        vector<int> v;
-        for(int i=temperatures.size()-1;i>=0;i--){
-            if(s.size()==0){v.push_back(0);}
-            if(s.size()!=0 && s.top().first>temperatures[i])
-            {v.push_back(s.top().second-i);}
-            else if(s.size()!=0 && s.top().first<=temperatures[i]){
-                while(s.size()!=0 && s.top().first<=temperatures[i]){
-                    s.pop();
-                }
-                if(s.size()==0){v.push_back(0);}
-                else {v.push_back(s.top().second-i);}
-            }
+        //Stack Next Greater element to right
+      int n=temperatures.size();
+        vector<int> res;
+     stack<pair<int,int>> s;
+        for(int i=n-1;i>=0;i--){
+            if(s.size()==0) res.push_back(0);
+            else if(s.size()!=0 and s.top().first>temperatures[i]) res.push_back(s.top().second-i);
+             else if(s.size()!=0 and s.top().first<=temperatures[i]){
+                 while(s.size()>0 and s.top().first<=temperatures[i]){ s.pop();
+                                                               }
+                 if(s.size()==0) res.push_back(0);
+                 else{res.push_back(s.top().second-i);
+                     }
+                 
+             }
             s.push({temperatures[i],i});
         }
-       
-        reverse(v.begin(),v.end());
-         return v;
-    }
-// vector<int> dailyTemperatures(vector<int>& T)
-// {
-// stack<pair<int,int>>st;
-// vector<int> v;
-// int i;
-// for( i=T.size()-1;i>=0;i--)
-// {
-// if(st.size()==0)
-// {
-// v.push_back(0);
-// }
-// else if(st.size()!=0 && st.top().first>T[i])
-// {
-// v.push_back(st.top().second-i);
-// }
-// else if(st.size()!= 0 && st.top().first<=T[i])
-// {
-// while(st.size()!=0 && st.top().first<=T[i])
-// {
-// st.pop();
-// }
-// if(st.size()==0)
-// {
-// v.push_back(0);
-// }
-// else
-// {
-// v.push_back(st.top().second-i);
-// }
-// }
-// st.push({T[i],i});
-// }
-
-//     reverse(v.begin(),v.end());
-//     return v;
-// }
+        reverse(res.begin(),res.end());     
+    
+    return res;}
 };
