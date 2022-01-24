fb60d95d3de929f96a7df404410fe3e8b8ca072c
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-01-24 22:46:05 +0530

    Time: 24 ms (48.30%), Space: 9.1 MB (33.83%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -15,6 +15,9 @@ public:
                     if(mp[s[i]]==0) mp.erase(s[i]);
                     i++;
                 }
+                 if(mp.size()==j-i+1){
+                res=max(res,j-i+1);
+            }
             }
             j++;
         }
