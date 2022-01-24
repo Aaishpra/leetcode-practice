e0ea77a95fc758fa1a8a8d651aa354d867bfa19f
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-01-24 13:21:13 +0530

    Time: 11 ms (21.16%), Space: 13.8 MB (32.22%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         int n=nums.size(), l=1,r=1, res=nums[0];
-        
+        //Kdane algo for product
         for(int i=0;i<n;i++){
             l=l*nums[i];
             r=r*nums[n-1-i];
