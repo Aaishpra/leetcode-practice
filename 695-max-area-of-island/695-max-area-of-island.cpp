1cb89fddff1c6d41009089795dd86ada57d5b7da
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-01-28 20:03:02 +0530

    Time: 20 ms (71.03%), Space: 23.3 MB (66.75%) - LeetHub

---
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    int dfs(vector<vector<int>> &grid, int i,int j){
+    if(i>=0 and i<grid.size() and j>=0 and j<grid[0].size() and grid[i][j]==1){
+        grid[i][j]=0;
+        int d1=dfs(grid,i+1,j);
+        int d2=dfs(grid,i-1,j);
+        int d3=dfs(grid,i,j+1);
+        int d4=dfs(grid,i,j-1);
+        
+        return 1+d1+d2+d3+d4;
+    }
+        return 0;
+    }
+    int maxAreaOfIsland(vector<vector<int>>& grid) {
+        int res=0;
+        for(int i=0;i<grid.size();i++){
+            for(int j=0;j<grid[0].size();j++){
+                if(grid[i][j]==1){
+                    res=max(res,dfs(grid,i,j));
+                }
+            }
+        }
+        return res;
+    }
+};
