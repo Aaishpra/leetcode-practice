f584ec48f78d63e173a26ab25adc9833ce382b67
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-01-29 20:42:02 +0530

    Time: 386 ms (55.64%), Space: 124.3 MB (44.21%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -1,24 +1,24 @@
 class Solution {
 public:
-    int dfs(vector<vector<int>> &graph,vector<int> &informTime,int node){
+    int dfs(vector<vector<int>> &graph,vector<int> &informTime,int node,vector<int>& vis){
         int time=0;
-        //vis[node]=1;
+        vis[node]=1;
         for(int child:graph[node]){
-          //  if(vis[child]==0){
-                time=max(time,dfs(graph,informTime,child));
-            //}
+          if(vis[child]==0){
+                time=max(time,dfs(graph,informTime,child,vis));
+            }
         }
         return informTime[node]+time;
     }
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-       // vector<int> vis(n,0);
+       vector<int> vis(n,0);
         vector<vector<int>> graph(n);
         for(int i=0;i<n;i++){
             if(manager[i]!=-1){
                 graph[manager[i]].push_back(i);
             }
         }
-        int time= dfs(graph,informTime,headID);
+        int time= dfs(graph,informTime,headID,vis);
         return time;
     }
 };
