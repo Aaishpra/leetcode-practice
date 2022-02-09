30bc93270a44b68f0e2e4d6dd9569a46eef69a11
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-02-09 16:45:37 +0530

    Time: 7 ms (83.80%), Space: 12.2 MB (75.84%) - LeetHub

--- 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.cpp
+++ 2164-sort-even-and-odd-indices-independently/2164-sort-even-and-odd-indices-independently.cpp
@@ -4,18 +4,26 @@ public:
         int n=arr.size();
         vector<int> odd,even;
         for(int i=0;i<arr.size();i++){
-            if(i%2==1)even.push_back(arr[i]);
+            if(i%2==0)even.push_back(arr[i]);
             else odd.push_back(arr[i]);
         }
-        sort(odd.begin(),odd.end());
-        sort(even.begin(),even.end(),greater<int>());
+        sort(even.begin(),even.end());
+        sort(odd.begin(),odd.end(),greater<int>());
         int i=0,j=0;
         vector<int> ans;
-        while(i<odd.size() or j<even.size()){
-            if(i<odd.size()) ans.push_back(odd[i++]);
-            if(j<even.size()) ans.push_back(even[j++]);
-        }
+        for(int i = 0; i<n; i+=2) {
+		arr[i] = even[i/2];
+	}
+	//filling odd spaces
+	for(int i = 1; i<n; i+=2) {
+		arr[i] = odd[i/2];
+	}
+//         while(i<odd.size() or j<even.size()){
+//             if(i<odd.size()) ans.push_back(odd[i++]);
+//             if(j<even.size()) ans.push_back(even[j++]);
+//         }
     
-        return ans;
+//         return ans;
+        return arr;
     }
 };
