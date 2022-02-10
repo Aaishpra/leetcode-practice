177e25ba4c7dcb007f5a4abb76c677d2a4dbad85
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-02-10 12:54:56 +0530

    Time: 60 ms (95.21%), Space: 20 MB (66.98%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -6,6 +6,7 @@ public:
         sort(nums.begin(),nums.end());
         int n=nums.size();
         for(int i=0;i<n-2;i++){
+            //Duplicates na aajye same number m
              if(i>0 && (nums[i]==nums[i-1]) )continue;
             int target=0-nums[i];
             int lo=i+1, hi=n-1;
@@ -14,6 +15,7 @@ public:
                 else if(nums[lo]+nums[hi]>target)hi--;
                 else {
                     res.push_back({nums[i],nums[lo],nums[hi]});
+                    //Lo ar hi k duplicates htane k liye
                     while(lo+1<hi and nums[lo]==nums[lo+1])lo++;
                     while(lo<hi-1 and nums[hi]==nums[hi-1])hi--;
                     lo++,hi--;
