f91baecb7de3fd8fca42906162739dcb4eaaa398
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-02-15 19:32:28 +0530

    Time: 43 ms (18.65%), Space: 20 MB (16.80%) - LeetHub

---
+++ 136-single-number/136-single-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+     unordered_map<int,int> mp;
+        for(int num:nums){
+            mp[num]++;
+        }
+        for(auto i:mp){
+            if(i.second==1)return i.first;
+        }
+        return 0;
+    }
+};
