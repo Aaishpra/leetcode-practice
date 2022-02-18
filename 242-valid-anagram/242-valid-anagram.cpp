22f5f023488b9bd72de7e9701b0566daf44dbcee
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-02-18 19:54:14 +0530

    Time: 15 ms (44.70%), Space: 7.5 MB (8.27%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> mp;
+        
+        for(char x:s){
+            mp[x]++;
+        }
+        for(char x:t){
+            mp[x]--;
+        }
+        for(auto x:mp){
+            if(x.second)return false;
+        }
+        return true;
+    
+    }
+};
