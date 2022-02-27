28a04e1ca9e5a49d62339ba69c8a48e636855baa
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-02-27 19:30:58 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (89.67%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -4,12 +4,12 @@
 class Solution {
 public:
     int firstBadVersion(int n) {
-     int lo = 1, hi = n, mid;
-        while (lo < hi) {
-            mid = lo + (hi - lo) / 2;
-            if (isBadVersion(mid)) hi = mid;
-            else lo = mid+1;
+        int lo=1, hi=n;
+        while(lo<hi){
+            int mid=lo+(hi-lo)/2;
+            if(isBadVersion(mid))hi=mid;
+            else lo=mid+1;
         }
-        return lo;   
+        return lo;
     }
 };
