36da7e6a0d1a99dfe66ee61c59f5c192268a35be
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-02-27 22:10:56 +0530

    Time: 8 ms (99.22%), Space: 15.8 MB (91.96%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
+    //FIND CEIL OF ELEMENT
     char nextGreatestLetter(vector<char>& letters, char target) {
-        char res=letters[0];
+      char res=letters[0];
         int lo=0;
         int hi=letters.size()-1;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
-            if(letters[mid]==target){
-                lo=mid+1;
-            }
-            else if(letters[mid]<target){
+            if(letters[mid]<=target){
                 lo=mid+1;
             }
             else if(letters[mid]>target){
