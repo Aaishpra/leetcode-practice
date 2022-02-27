eda249210008490a8dd6a3e0de5cf4020ddbb49b
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-02-27 21:51:00 +0530

    Time: 0 ms (100.00%), Space: 6 MB (26.15%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,16 +1,6 @@
 class Solution {
 public:
     int arrangeCoins(int n) {
-        long left=0;
-        int right=n;
-        
-        while(left<=right){
-            long mid=left+(right-left)/2;
-            long curr=mid*(mid+1)/2;
-            if(n==curr)return int(mid);
-            else if(curr>n)right=mid-1;
-            else left=mid+1;
-        }
-        return right;
+      return floor(-0.5+sqrt((double)2*n+0.25));
     }
 };
