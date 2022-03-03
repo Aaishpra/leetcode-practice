e796822619edb6b6ed10c1083a0cd528f63ad4ca
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-03-03 18:19:48 +0530

    Time: 19 ms (75.43%), Space: 18.6 MB (50.39%) - LeetHub

--- 275-h-index-ii/275-h-index-ii.cpp
+++ 275-h-index-ii/275-h-index-ii.cpp
@@ -1,14 +1,20 @@
 class Solution {
 public:
     int hIndex(vector<int>& citations) {
-         int left=0, len = citations.size(), right= len-1,  mid;
-        while(left<=right)
-        {
-            mid=left+(right-left)/2;
-            if(citations[mid]== (len-mid)) return citations[mid];
-            else if(citations[mid] > (len-mid)) right = mid - 1;
-            else left = mid + 1;
+        int lo=0;
+        int len=citations.size();
+        int hi=citations.size()-1;
+       // if(citations.size()==1)return citations[0];
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(len-mid==citations[mid]){
+                return citations[mid];
+            }
+            else if(len-mid<citations[mid]){
+                hi=mid-1;
+            }
+            else lo=mid+1;
         }
-        return len - (right+1);
+        return len-(hi+1);
     }
 };
