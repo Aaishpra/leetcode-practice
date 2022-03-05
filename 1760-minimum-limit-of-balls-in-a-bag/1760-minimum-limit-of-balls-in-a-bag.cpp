6c34af5269ef1286664dfca30209ef48db694089
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-03-05 09:41:15 +0530

    Time: 273 ms (58.04%), Space: 56 MB (37.74%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,30 +1,24 @@
 class Solution {
 public:
-     int minimumSize(vector<int>& nums, int maxOperations) {
-		int start = 1, end = *max_element(nums.begin(), nums.end());
-		int minPenalty = end;
-		// binary search on possible range
-		while (start <= end) {
-			int penalty = start + (end-start)/2;
-			if (isPossible(nums, maxOperations, penalty))
-				minPenalty = penalty, end = penalty-1;
-			else
-				start = penalty+1;
-		}
-
-		return minPenalty;
-	}
-
-	bool isPossible(vector<int>& nums, int maxOperations, int penalty) {
-		int requiredOps = 0;
-		for (int n : nums) {
-			// no. of operations required to bring n less than or eq to curr assumed penalty
-			int x = n / penalty;
-			// if n is divisible by penalty, need to subtract 1
-			if (n % penalty == 0) x--;
-			requiredOps += x;
-		}
-		// getting current penalty is possible only if required ops is <= maxOps
-		return requiredOps <= maxOperations;
+    bool isValid(vector<int>& nums,int m,int mid){
+        int x=0;
+        for(int n:nums){
+            x+=n/mid;
+            if(n%mid==0)x--;
+        }
+        return x<=m;
+    }
+    int minimumSize(vector<int>& nums, int maxOperations) {
+    int lo=1, hi=*max_element(nums.begin(),nums.end());
+        int res=0;
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(isValid(nums,maxOperations,mid)){
+                res=mid;
+                hi=mid-1;
+            }
+            else lo=mid+1;
+        }
+        return res;
     }
 };
