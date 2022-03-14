53224ac9ba8f3e1878e512c406ad3003e43af512
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-03-14 10:19:55 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (54.79%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -3,7 +3,7 @@ public:
     int climbStairs(int n) {
        if(n<=2)return n;
         int f=0,f1=1,f2=2;
-        for(int i=2;i<n;i++){
+        for(int i=3;i<=n;i++){
             f=f1+f2;
             f1=f2;
             f2=f;
