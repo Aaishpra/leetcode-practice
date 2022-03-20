b69bb81761b065acafab6737ac03f49e540550fe
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-03-20 10:31:37 +0530

    Time: 0 ms (100.00%), Space: 6 MB (15.96%) - LeetHub

--- 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
+++ 357-count-numbers-with-unique-digits/357-count-numbers-with-unique-digits.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     int countNumbersWithUniqueDigits(int n) {
-        if(n==0) return 1;
-        int ans=10; // for 0 digit = 1 and for 1 digit  unique digits = 9 so total = U(0) + U(1) = 10
-        int start=9;
-        int current=9;
-        
+        if(n==0)return 1;
+        int ans=10;
+        int start=9,cur=9;
         while(n-->1 and start){
-            current*=start;
+            cur*=start;
             start--;
-            ans+=current;
+            ans+=cur;
         }
         return ans;
     }
