e614b40f3b2e6021972f9a54e57f763794979adb
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-04-14 12:38:19 +0530

    Time: 3 ms (35.80%), Space: 5.8 MB (90.94%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,11 +1,13 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        //kind of dp
+        // Dp on grid
         int t[m][n];
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 or j==0)t[i][j]=1;
+                if(i==0 or j==0) {
+                    t[i][j]=1;
+                }
                 else t[i][j]=t[i-1][j]+t[i][j-1];
             }
         }
