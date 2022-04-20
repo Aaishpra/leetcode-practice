5efcb1cc1f0e263f9066934ed8af51168f4cf4aa
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-04-20 22:50:03 +0530

    Time: 108 ms (63.22%), Space: 19.8 MB (96.24%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-      vector<vector<int>> res;
-        sort(nums.begin(),nums.end());
+     if(nums.size()<3)return {};
+        vector<vector<int>> res;
         int n=nums.size();
-        for(int i=0;i<n-2;i++){
-            //Duplicates na aajye same number m
-             if(i>0 && (nums[i]==nums[i-1]) )continue;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<nums.size()-2;i++){
+            if(i>0 and nums[i]==nums[i-1])continue;
             int target=0-nums[i];
-            int lo=i+1, hi=n-1;
+            int lo=i+1,hi=n-1;
             while(lo<hi){
-                if(nums[lo]+nums[hi]<target) lo++;
+                if(nums[lo]+nums[hi]<target)lo++;
                 else if(nums[lo]+nums[hi]>target)hi--;
                 else {
-                    res.push_back({nums[i],nums[lo],nums[hi]});
-                    //Lo ar hi k duplicates htane k liye
+                    res.push_back({nums[lo],nums[hi],nums[i]});
                     while(lo+1<hi and nums[lo]==nums[lo+1])lo++;
-                    while(lo<hi-1 and nums[hi]==nums[hi-1])hi--;
-                    lo++,hi--;
+                    while(lo+1<hi and nums[hi]==nums[hi-1])hi--;
+                    lo++;
+                    hi--;
                 }
             }
         }
