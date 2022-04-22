6496ad59d9b03d08897d9a67b61dfec098c944e7
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-04-22 07:27:19 +0530

    Time: 534 ms (24.74%), Space: 189.7 MB (12.00%) - LeetHub

--- 239-sliding-window-maximum/239-sliding-window-maximum.cpp
+++ 239-sliding-window-maximum/239-sliding-window-maximum.cpp
@@ -1,30 +1,36 @@
 class Solution {
 public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
-        vector<int> result;
-        deque<int> dq;
+    vector<int> ans;
+        list<int> l;
+        int i=0;
+        int j=0;
         
-        int i = 0;
-        int j = 0;
+        if (k>nums.size()) // edge case
+        {
+            ans.push_back(*max_element(nums.begin(),nums.end()));
+            return ans;
+        }
         
-        while (j < nums.size()) {
-            while (dq.size() > 0 && dq.back() < nums[j]) {
-                dq.pop_back();
-            }   
-            dq.push_back(nums[j]);
-            if (j - i + 1 < k) {
-                j++;
+        while (j<nums.size())
+        {
+            while(l.size()>0 && l.back() <nums[j])
+            {
+                l.pop_back();
             }
-            else if (j - i + 1 == k) {
-                result.push_back(dq.front());
-                if (nums[i] == dq.front()) {
-                    dq.pop_front();
-                }
+            l.push_back(nums[j]);
+            if ((j-i+1)<k)
+                j++;
+            else if (j-i+1==k)
+            {
+                ans.push_back(l.front());
+                if (l.front()==nums[i])
+                    l.pop_front();
                 i++;
                 j++;
             }
-        }      
-    
-        return result;
+            
+        }
+        return ans;
     }
 };
