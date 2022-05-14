0590cc93026bf9595251d294e9ad01ea31a0e12f
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-05-14 15:18:18 +0530

    Time: 23 ms (45.90%), Space: 8.7 MB (72.75%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.cpp
@@ -1,29 +1,29 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        unordered_map<int,int> mp;
-        for(char c:p) mp[c]++;
+        unordered_map<char, int> mp;
+        int i=0, j=0;
+        for(char c:p){
+            mp[c]++;
+        }
         int cnt=mp.size();
         int n=s.size();
         int k=p.size();
         vector<int> ans;
-        int i=0,j=0;
         while(j<n){
             if(mp.find(s[j])!=mp.end()){
                 mp[s[j]]--;
                 if(mp[s[j]]==0) cnt--;
             }
+            //if(j-i+1<k) j++;
             if(j-i+1==k){
                 if(cnt==0) ans.push_back(i);
-                if(mp.find(s[i])!=mp.end()){
-                mp[s[i]]++;
-                if(mp[s[i]]==1) cnt++;
-                   
-            }
-                i++;
+                if(mp.find(s[i])!=mp.end()){mp[s[i]]++;
+                if(mp[s[i]]==1) cnt++;}
+                i++;}
+                j++;
+            
         }
-            j++;
-        }
-            return ans;
+        return ans;
     }
 };
