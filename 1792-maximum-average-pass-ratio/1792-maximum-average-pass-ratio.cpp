cc8be88ab66011590ff6f336ae3ed04bbfa770c7
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-05-20 08:52:01 +0530

    Time: 1311 ms (41.67%), Space: 93.6 MB (43.91%) - LeetHub

--- 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
+++ 1792-maximum-average-pass-ratio/1792-maximum-average-pass-ratio.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    double maxAverageRatio(vector<vector<int>>& c, int e) {
+    double maxAverageRatio(vector<vector<int>>& classes, int extraStudents) {
+        int n=classes.size();
         priority_queue<pair<double,pair<int,int>>> pq;
-        for(int i=0;i<c.size();i++){
-            double delta=double(c[i][0]+1)/(c[i][1]+1)-(double)(c[i][0])/(c[i][1]);
-            pq.push({delta,{c[i][0],c[i][1]}});
+        for(int i=0;i<n;i++){
+            int pass=classes[i][0];
+            int total=classes[i][1];
+            double delta=(double)(pass+1)/(total+1)-(double)(pass)/(total);
+            pq.push({delta,{pass,total}});
         }
-       while(e--){
-           int p=pq.top().second.first;
-           int t=pq.top().second.second;
-           p++;
-           t++;
-           double newdelta=(double)(p+1)/(t+1)-(double)(p)/(t);
-           pq.pop();
-           pq.push({newdelta,{p,t}});
-       }
         
-        double ratio=0;
+        while(extraStudents--){
+            int pass=pq.top().second.first;
+            int total=pq.top().second.second;
+            pass++;
+            total++;
+            double newdelta=(double)(pass+1)/(total+1)-(double)(pass)/(total);
+            pq.pop();
+                pq.push({newdelta,{pass,total}});
+        }
+        double avg=0;
         while(!pq.empty()){
-            ratio+=(double)(pq.top().second.first)/(pq.top().second.second);
+            avg+=(double)(pq.top().second.first)/(pq.top().second.second);
             pq.pop();
         }
-        return ratio/c.size();
-    }
+        return avg/n;}
 };
