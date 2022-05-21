514379e40a28c4ed55e92be290c2adb2e1055676
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-05-21 13:38:42 +0530

    Time: 21 ms (12.46%), Space: 12.2 MB (12.16%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<string> ans;
+    void solve(string ip,string op){
+        if(ip.length()==0){
+            ans.push_back(op);
+            return;
+        }
+        string op1=op;
+        string op2=op;
+        if(isalpha(ip[0])){
+        op1.push_back(tolower(ip[0]));
+        op2.push_back(toupper(ip[0]));
+        ip.erase(ip.begin()+0);
+        solve(ip,op1);
+        solve(ip,op2);
+        }
+        else {
+            string op1=op;
+            op1.push_back(ip[0]);
+            ip.erase(ip.begin()+0);
+            solve(ip,op1);
+            
+        }
+        return;
+    }
+    vector<string> letterCasePermutation(string ip) {
+        string op="";
+        solve(ip,op);
+        return ans;
+    }
+};
