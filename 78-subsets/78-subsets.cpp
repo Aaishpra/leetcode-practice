a2d4fa3830147fe06566da89d907f1e83fc7cd57
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-05-21 14:44:57 +0530

    Time: 7 ms (26.62%), Space: 17.3 MB (6.66%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,22 +1,22 @@
 class Solution {
 public:
-     vector<vector<int>> ans;
-    
-    void sub(vector<int> &nums, int i, vector<int> temp)
-    {
-        if(i==nums.size())
-        {
-            ans.push_back(temp);
+    vector<vector<int>> ans;
+    void solve(vector<int> ip,vector<int> op){
+        if(ip.size()==0){
+            ans.push_back(op);
             return;
         }
-        
-        sub(nums, i+1, temp);
-        temp.push_back(nums[i]);
-        sub(nums, i+1, temp);
+        vector<int> op1=op;
+        vector<int> op2=op;
+        op1.push_back(ip[0]);
+        ip.erase(ip.begin()+0);
+        solve(ip,op1);
+        solve(ip,op2);
+        return;
     }
-    
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<int> temp;       
-        sub(nums, 0, temp); // or sub(nums, 0, vector<int> {});
-        return ans;}
+        vector<int> op={};
+        solve(nums,op);
+        return ans;
+    }
 };
