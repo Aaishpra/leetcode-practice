412bba4b11cf119674f9d63157448a06ecb86ec4
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-01 13:50:08 +0530

    Time: 64 ms (91.79%), Space: 21.7 MB (86.16%) - LeetHub

--- 1020-number-of-enclaves/1020-number-of-enclaves.cpp
+++ 1020-number-of-enclaves/1020-number-of-enclaves.cpp
@@ -1,16 +1,40 @@
-class Solution {
-    public:
-   void dfs(vector<vector<int>>& A, int i, int j) {
-  if (i < 0 || j < 0 || i == A.size() || j == A[i].size() || A[i][j] != 1) return;
-  A[i][j] = 0;
-  dfs(A, i + 1, j), dfs(A, i - 1, j), dfs(A, i, j + 1), dfs(A, i, j - 1);
-}
-int numEnclaves(vector<vector<int>>& A) {
-  for (auto i = 0; i < A.size(); ++i)
-    for (auto j = 0; j < A[0].size(); ++j) 
-      if (i * j == 0 || i == A.size() - 1 || j == A[i].size() - 1) dfs(A, i, j);
+ class Solution {
+ 	int rows, cols;
+ 	void dfs(vector<vector<int>>& A, int i, int j) {
+ 		if (i < 0 || j < 0 || i >= rows || j >= cols)
+ 			return;
 
-  return accumulate(begin(A), end(A), 0, [](int s, vector<int> &r)
-    { return s + accumulate(begin(r), end(r), 0); });
-}
-};
+ 		if (A[i][j] != 1) 
+ 			return;
+
+ 		A[i][j] = -1;
+ 		dfs(A, i+1, j);
+ 		dfs(A, i-1, j);
+ 		dfs(A, i, j+1);
+ 		dfs(A, i, j-1);
+ 	}
+ public:
+ 	int numEnclaves(vector<vector<int>>& A) {
+
+ 		if (A.empty()) return 0;
+
+ 		rows = A.size();
+ 		cols = A[0].size();
+ 		for (int i = 0; i < rows; i++) {
+ 			for (int j = 0; j < cols; j++) {
+ 				if (i == 0 || j == 0 || i == rows-1 || j == cols-1)
+ 					dfs(A, i, j);
+ 			}
+ 		}
+
+ 		int ans = 0;
+ 		for (int i = 0; i < rows; i++) {
+ 			for (int j = 0; j < cols; j++) {
+ 				if (A[i][j] == 1)
+ 					ans++;
+ 			}
+ 		}
+
+ 		return ans;
+ 	}
+ };
