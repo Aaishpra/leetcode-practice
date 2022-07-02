056ea68181c1485edac2b510a582aea240448186
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-02 20:48:57 +0530

    Time: 103 ms (50.50%), Space: 34.8 MB (6.04%) - LeetHub

--- 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
+++ 1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts/1465-maximum-area-of-a-piece-of-cake-after-horizontal-and-vertical-cuts.cpp
@@ -7,23 +7,23 @@ public:
         sort(wc.begin(),wc.end());
         int n1=hc.size(), n2=wc.size();
         vector<int> height;
-        vector<int> weight;
+        vector<int> width;
         height.push_back(abs(h-hc[n1-1]));
-        height.push_back(abs(0-hc[0]));
+        height.push_back(abs(hc[0]));
         
-        weight.push_back(abs(w-wc[n2-1]));
-        weight.push_back(abs(0-wc[0]));
+        width.push_back(abs(w-wc[n2-1]));
+        width.push_back(abs(wc[0]));
         
         for(int i=1;i<n1;i++){
             height.push_back(abs(hc[i-1]-hc[i]));
         }
          for(int i=1;i<n2;i++){
-            weight.push_back(abs(wc[i-1]-wc[i]));
+            width.push_back(abs(wc[i-1]-wc[i]));
         }
-        long long int a = *max_element(height.begin(), height.end());
-        long long int b = *max_element(weight.begin(), weight.end());
+        long long int mh = *max_element(height.begin(), height.end());
+        long long int mw = *max_element(width.begin(), width.end());
         
-        // Multiply and return
-        return (int)(a%mod*b%mod);
+        
+        return (int)(mh%mod*mw%mod);
     }
 };
