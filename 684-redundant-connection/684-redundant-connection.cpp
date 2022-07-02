634ec124f7807a134a054c71fa0a374abf79337f
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-02 06:59:16 +0530

    Time: 11 ms (63.13%), Space: 8.8 MB (68.87%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,55 +1,47 @@
-class UnionFind {
-public:
-    UnionFind(int sz) : root(sz), rank(sz) {
-        for (int i = 0; i < sz; i++) {
-            root[i] = i;
-            rank[i] = 1;
+class UnionFind{
+    private:
+    vector<int> root;
+    vector<int> rank;
+    public:
+    UnionFind(int sz):root(sz),rank(sz){
+        for(int i=0;i<sz;i++){
+            root[i]=i;
+            rank[i]=1;
         }
     }
-
-    int find(int x) {
-        if (x == root[x]) {
-            return x;
-        }
-        return root[x] = find(root[x]);
+    int find(int x){
+        if(x==root[x]) return x;
+        return root[x]=find(root[x]);
     }
-
-    bool unionSet(int x, int y) {
-        int rootX = find(x);
-        int rootY = find(y);
-        if (rootX != rootY) {
-            if (rank[rootX] > rank[rootY]) {
-                root[rootY] = rootX;
-            } else if (rank[rootX] < rank[rootY]) {
-                root[rootX] = rootY;
-            } else {
-                root[rootY] = rootX;
-                rank[rootX] += 1;
+    bool find(int x,int y){
+        int rootx=find(x);
+        int rooty=find(y);
+        if(rootx!=rooty){
+            if(rank[rootx]>rank[rooty]){
+                root[rooty]=rootx;
+            }
+            else if(rank[rooty]>rank[rootx]){
+                root[rootx]=rooty;
+            }
+            else {
+                root[rooty]=rootx;
+                rank[rootx]+=1;
             }
             return true;
         }
         return false;
     }
-
-    bool connected(int x, int y) {
-        return find(x) == find(y);
+    bool isconnected(int x,int y){
+        return find(x)==find(y);
     }
-
-private:
-    vector<int> root;
-    vector<int> rank;
 };
-
-
-
-
 class Solution {
 public:
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-     int n=edges.size();
-      UnionFind uf(n+1);
+        int n=edges.size();
+        UnionFind uf(n+1);
         for(auto e:edges){
-            if(!uf.unionSet(e[0],e[1]))return e;
+            if(uf.find(e[0],e[1])==false) return e;
         }
         return {};
     }
