e3d9c08703d6bd581bba417cf177939a1db15f75
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-03 21:53:41 +0530

    Time: 47 ms (21.08%), Space: 14.1 MB (47.09%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,33 +1,31 @@
 class Solution {
 public:
-    vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+     vector<vector<int>> ar(n);
         vector<int> res;
-        vector<int> in(numCourses,0);
-        vector<vector<int>> ar(numCourses);
-       //adj list for directed graph 
-        for(auto p:prerequisites){
+        vector<int> in(n,0);
+        
+        queue<int> q;
+        
+        for(auto p:pre){
             ar[p[1]].push_back(p[0]);
             in[p[0]]++;
         }
         
-        queue<int> q;
-        for(int i=0;i<numCourses;i++){
-            if(in[i]==0)q.push(i);
+        for(int i=0;i<n;i++){
+            if(in[i]==0) q.push(i);
         }
         
         while(!q.empty()){
             int cur=q.front();
             res.push_back(cur);
             q.pop();
-            
-            for(auto child:ar[cur]){
+            for(int child:ar[cur]){
                 in[child]--;
-                if(in[child]==0)
-                    q.push(child);
+                if(in[child]==0) q.push(child);
             }
         }
-            if(res.size()==numCourses)
-                return res;
+        if(res.size()==n) return res;
         return {};
     }
 };
