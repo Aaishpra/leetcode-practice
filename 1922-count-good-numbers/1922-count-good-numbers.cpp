f8cd24d745a14b0f5a5494191257148130c5ac4f
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-06 06:53:45 +0530

    Time: 0 ms (100.00%), Space: 6 MB (44.34%) - LeetHub

--- 1922-count-good-numbers/1922-count-good-numbers.cpp
+++ 1922-count-good-numbers/1922-count-good-numbers.cpp
@@ -11,9 +11,9 @@ public:
         return ans;
     }
     int countGoodNumbers(long long n) {
-       long long odd=n/2;
         long long even=n/2+n%2;
-        long long ans= (power(5,even)*power(4,odd))%mod;
+        long long odd=n/2;
+        long long ans=(power(5,even)*power(4,odd))%mod;
         return ans;
     }
 };
