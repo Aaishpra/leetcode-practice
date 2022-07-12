5966d46a67077b8b88a677c5fbf69005e0423253
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-12 19:01:56 +0530

    Time: 294 ms (52.45%), Space: 17.8 MB (48.63%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,29 +1,28 @@
-
 class Solution {
 public:
-    bool isSubset(vector<int> &nums,int sum){
+    bool isSubset(vector<int>& nums,int sum){
         int n=nums.size();
-        bool t[n+1][sum+1];
+        int dp[n+1][sum+1];
+        
         for(int i=0;i<n+1;i++){
             for(int j=0;j<sum+1;j++){
-                if(i==0)t[i][j]=false;
-                if(j==0)t[i][j]=true;    
+                if(i==0) dp[i][j]=false;
+                else if(j==0) dp[i][j]=true;
             }
         }
         
         for(int i=1;i<n+1;i++){
             for(int j=1;j<sum+1;j++){
                 if(nums[i-1]<=j){
-                    t[i][j]=t[i-1][j-nums[i-1]] or t[i-1][j];
+                    dp[i][j]=dp[i-1][j-nums[i-1]]|dp[i-1][j];
                 }
-                else t[i][j]=t[i-1][j];
+                else dp[i][j]=dp[i-1][j];
             }
         }
-        return t[n][sum];
+        return dp[n][sum];
     }
     bool canPartition(vector<int>& nums) {
-    int sum=accumulate(nums.begin(),nums.end(),0);
-        if(sum%2!=0)return 0;
-        return isSubset(nums,sum/2);
+        int sum=accumulate(nums.begin(),nums.end(),0);
+        return sum%2==0?isSubset(nums,sum/2):0;
     }
 };
