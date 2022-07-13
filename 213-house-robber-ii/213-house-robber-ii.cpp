b85755e56d9cba51fc4d5316f9853ee105bf07d7
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-13 17:16:15 +0530

    Time: 0 ms (100.00%), Space: 7.9 MB (41.88%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.cpp
+++ 213-house-robber-ii/213-house-robber-ii.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-     public:
     int rob(vector<int>& nums) {
-        int n = nums.size(); 
-        if (n < 2) return n ? nums[0] : 0;
-        return max(robber(nums, 0, n - 2), robber(nums, 1, n - 1));
+     //two times house robber;
+        if(nums.size()==1)return nums[0];
+        
+        vector<int> numsA(nums.begin() + 1, nums.end());
+        vector<int> numsB(nums.begin(), nums.end()-1);
+        
+        return max(solve(numsA),solve(numsB));
     }
-private:
-    int robber(vector<int>& nums, int l, int r) {
-        int pre = 0, cur = 0;
-        for (int i = l; i <= r; i++) {
-            int temp = max(pre + nums[i], cur);
-            pre = cur;
-            cur = temp;
+    int solve(vector<int> &nums){
+        vector<int> dp(nums);
+        int n=nums.size();
+            if(n==1)return nums[0];
+        dp[1]=max(nums[1],nums[0]);
+        
+        for(int i=2;i<n;i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
         }
-        return cur;
+        return dp[n-1];
     }
-   
 };
