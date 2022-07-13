d3a1334f1f2a695710199654592c5151b1749eaf
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-13 17:05:43 +0530

    Time: 3 ms (54.07%), Space: 7.9 MB (24.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
+    //Memoization
     int rob(vector<int>& nums) {
-        vector<int> dp(nums);
-        int n=nums.size();
-        if(n==1) return nums[0];
-        dp[1]=max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
-        }
-        return dp[n-1];
+        vector<int> dp(nums.size(),-1);
+        return solve(nums,dp,0);
+    }
+    int solve(vector<int>& nums,vector<int>& dp,int i){
+        if(i>=nums.size())return 0;
+        if(dp[i]!=-1)return dp[i];
+        return dp[i]=max(nums[i]+solve(nums,dp,i+2),solve(nums,dp,i+1));
     }
 };
