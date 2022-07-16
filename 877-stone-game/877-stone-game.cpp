4eae83ed6c42844892ec149e70aa24120d8be73b
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-16 16:01:23 +0530

    Time: 4 ms (71.38%), Space: 7.6 MB (70.68%) - LeetHub

--- 877-stone-game/877-stone-game.cpp
+++ 877-stone-game/877-stone-game.cpp
@@ -1,17 +1,6 @@
 class Solution {
 public:
-    int dp[501][501][2];
-    int solve(vector<int> & nums,int i,int j,int chance){
-        if(i>j)return 0;
-        if(dp[i][j][chance]!=-1)return dp[i][j][chance];
-        if(chance==0)return dp[i][j][chance]=max(nums[i]+solve(nums,i+1,j,1),nums[j]+solve(nums,i,j-1,1));
-        else return dp[i][j][chance]=min(solve(nums,i+1,j,0),solve(nums,i,j-1,0));
-    }
     bool stoneGame(vector<int>& piles) {
-        memset(dp,-1,sizeof(dp));
-        int sum=accumulate(piles.begin(),piles.end(),0);
-        int alice=solve(piles,0,piles.size()-1,0);
-        int bob=sum-alice;
-        return alice>bob;
+     return true;   
     }
 };
