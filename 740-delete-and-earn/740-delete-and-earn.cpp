11bee68f476ce2da464c7f2af74c31c79018f896
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-19 15:49:27 +0530

    Time: 125 ms (5.82%), Space: 56.6 MB (5.02%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,8 +1,8 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        int n=10001;
-        vector<int> sum(n,0);
+        int n=100001;
+       vector<int> sum(n,0);
         vector<int> dp(n,0);
         
         for(int num:nums){
@@ -12,7 +12,7 @@ public:
         dp[1]=sum[1];
         
         for(int i=2;i<n;i++){
-            dp[i]=max(dp[i-2]+sum[i],dp[i-1]);
+            dp[i]=max(dp[i-1],sum[i]+dp[i-2]);
         }
         return dp[n-1];
     }
