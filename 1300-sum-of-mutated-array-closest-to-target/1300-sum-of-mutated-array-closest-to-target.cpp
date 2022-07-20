1b64420e49cb22b805566086741b9a2653af194c
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-20 17:32:40 +0530

    Time: 30 ms (36.84%), Space: 12.5 MB (35.85%) - LeetHub

--- 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
+++ 1300-sum-of-mutated-array-closest-to-target/1300-sum-of-mutated-array-closest-to-target.cpp
@@ -1,6 +1,9 @@
 class Solution {
 public:
     int isValid(vector<int>& arr, int target,int mid,int n){
+        std::ios::sync_with_stdio(false);
+    std::cin.tie(nullptr);
+    std::cout.tie(nullptr);
         int sum=0;
         
         for(int i=0;i<n;i++){
