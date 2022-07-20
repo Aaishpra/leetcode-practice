202d5b89eb07a61dc2810acc7335d6d81cf791db
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-20 16:42:41 +0530

    Time: 113 ms (23.79%), Space: 26.1 MB (67.26%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -1,26 +1,21 @@
 class Solution {
 public:
-    bool isValid(vector<int>& arr,int curr_ans, int days){
-     int d=1;
+    bool isValid(vector<int>&weights,int mid,int days){
+        int d=1;
         int sum=0;
-        for(int i=0;i<arr.size();i++){
-            sum+=arr[i];
-            if(sum>curr_ans){
-                sum=arr[i];
+        for(int i=0;i<weights.size();i++){
+            sum+=weights[i];
+            if(sum>mid)
+            {sum=weights[i];
                 d++;
-            }
+        }
         }
         return d<=days;
     }
     int shipWithinDays(vector<int>& weights, int days) {
-        int sum=0;
-        int mx=INT_MIN;
-        for(int i=0;i<weights.size();i++){
-            sum+=weights[i];
-            mx=max(mx,weights[i]);
-        }
+        int lo=*max_element(weights.begin(),weights.end());
+        int hi=accumulate(weights.begin(),weights.end(),0);
         int res=0;
-        int lo=mx,hi=sum;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
             if(isValid(weights,mid,days)){
