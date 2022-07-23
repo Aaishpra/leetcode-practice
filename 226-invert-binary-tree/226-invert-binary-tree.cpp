b44dca2c13dde86225480a928134fe6d7f2d139b
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-23 17:51:29 +0530

    Time: 9 ms (9.37%), Space: 9.6 MB (80.35%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -12,11 +12,11 @@
 class Solution {
 public:
     TreeNode* invertTree(TreeNode* root) {
-        if(root){
-            invertTree(root->left);
-            invertTree(root->right);
-            swap(root->left,root->right);
-        }
+        if(!root) return NULL;
+        invertTree(root->left);
+        invertTree(root->right);
+        swap(root->left,root->right);
         return root;
+        
     }
 };
