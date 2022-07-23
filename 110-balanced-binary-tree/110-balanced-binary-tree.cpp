273fe3d7159a768e60f96ec0de3054597759ead0
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-23 14:28:50 +0530

    Time: 16 ms (72.42%), Space: 20.8 MB (81.07%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.cpp
+++ 110-balanced-binary-tree/110-balanced-binary-tree.cpp
@@ -10,24 +10,16 @@
  * };
  */
 class Solution {
-    public:
-      bool ans;
-    int checkBalance(TreeNode* root){
-        if(!root)
-            return 0;
-        if(!ans) // if Answer is already False then return it.
-            return 0;
-        int leftSubTree = checkBalance(root->left);
-        int rightSubTree = checkBalance(root->right);
-        if(abs(leftSubTree-rightSubTree) > 1){
-            ans = false;
-        }
-        return 1 + max(leftSubTree, rightSubTree);
+public:
+    //Top down approach O(n^2)
+    int depth(TreeNode* root){
+        if(root==NULL) return 0;
+        return max(depth(root->left),depth(root->right))+1;
     }
-    bool isBalanced(TreeNode* root){
-        ans = true;
-        int temp = checkBalance(root);
-        return ans;
+    bool isBalanced(TreeNode* root) {
+        if(root == NULL) return true;
+        int lh=depth(root->left);
+        int rh=depth(root->right);
+        return (abs(lh-rh)<=1) and isBalanced(root->left) and isBalanced(root->right);
     }
-
 };
