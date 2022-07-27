525192fc5d55608addf7a82a9ef202a2c774a98b
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-27 18:54:12 +0530

    Time: 11 ms (56.88%), Space: 12.1 MB (20.11%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -1,32 +1,32 @@
 class Solution {
 public:
-    vector<string> ans;
+    vector<string> res;
     void solve(string ip,string op){
         if(ip.length()==0){
-            ans.push_back(op);
+            res.push_back(op);
             return;
         }
         string op1=op;
         string op2=op;
         if(isalpha(ip[0])){
-        op1.push_back(tolower(ip[0]));
-        op2.push_back(toupper(ip[0]));
-        ip.erase(ip.begin()+0);
-        solve(ip,op1);
-        solve(ip,op2);
+
+            op1.push_back(toupper(ip[0]));
+            op2.push_back(tolower(ip[0]));
+            ip.erase(ip.begin()+0);
+            solve(ip,op1);
+            solve(ip,op2);
         }
-        else {
+        else{
             string op1=op;
             op1.push_back(ip[0]);
             ip.erase(ip.begin()+0);
             solve(ip,op1);
-            
         }
         return;
     }
-    vector<string> letterCasePermutation(string ip) {
+    vector<string> letterCasePermutation(string s) {
         string op="";
-        solve(ip,op);
-        return ans;
+        solve(s,op);
+        return res;
     }
 };
