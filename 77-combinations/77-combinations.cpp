9a95e35ea422ffdae9621c3e65a79071015f3213
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-28 10:32:23 +0530

    Time: 670 ms (13.87%), Space: 165.2 MB (6.12%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,19 +1,21 @@
 class Solution {
 public:
     vector<vector<int>> res;
-    void solve(int start,int end,vector<int>& temp,int k){
+    void backtrack(int start,int end,vector<int> temp,int k){
         if(temp.size()==k){
             res.push_back(temp);
+            return;
         }
+        
         for(int i=start;i<=end;i++){
             temp.push_back(i);
-            solve(i+1,end,temp,k);
+            backtrack(i+1,end,temp,k);
             temp.pop_back();
         }
     }
     vector<vector<int>> combine(int n, int k) {
         vector<int> temp;
-        solve(1,n,temp,k);
+        backtrack(1,n,temp,k);
         return res;
     }
 };
