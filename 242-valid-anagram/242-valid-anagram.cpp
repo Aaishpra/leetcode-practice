4143778e988e8ea63e2c1952d904b183dc1728ad
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-07-28 10:05:04 +0530

    Time: 24 ms (19.33%), Space: 7.2 MB (77.97%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,18 +1,12 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> mp;
-        
-        for(char x:s){
-            mp[x]++;
-        }
-        for(char x:t){
-            mp[x]--;
-        }
-        for(auto x:mp){
-            if(x.second)return false;
+       sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        if(s.size()!=t.size())return false;
+        for(int i=0;i<s.size();i++){
+            if(s[i]!=t[i]) return false;
         }
         return true;
-    
     }
 };
