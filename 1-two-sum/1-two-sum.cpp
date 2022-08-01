f23a0bb72924d8e8eed8b3e9d89cb29bf7fa76c4
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-01 15:37:58 +0530

    Time: 20 ms (64.07%), Space: 11 MB (33.52%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -7,7 +7,7 @@ public:
             int number=target-nums[i];
             if(mp.count(number)>0)
                 return {mp[number],i};
-            //Storing the element in key value pair of hashmap
+            
             mp[nums[i]]=i;
         }
         return {};
