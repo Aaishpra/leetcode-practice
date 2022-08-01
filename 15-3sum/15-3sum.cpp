3976d02278626f97d056bc85d32c4ce27d09379c
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-01 16:06:34 +0530

    Time: 81 ms (93.96%), Space: 20 MB (78.09%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
-     if(nums.size()<3)return {};
-        vector<vector<int>> res;
         int n=nums.size();
         sort(nums.begin(),nums.end());
+        vector<vector<int>> res;
         for(int i=0;i<nums.size()-2;i++){
-            if(i>0 and nums[i]==nums[i-1])continue;
-            int target=0-nums[i];
+            if(i>0 and nums[i]==nums[i-1]) continue;
+            int target=-nums[i];
             int lo=i+1,hi=n-1;
             while(lo<hi){
-                if(nums[lo]+nums[hi]<target)lo++;
+                if(nums[lo]+nums[hi]<target) lo++;
                 else if(nums[lo]+nums[hi]>target)hi--;
                 else {
-                    res.push_back({nums[lo],nums[hi],nums[i]});
-                    while(lo+1<hi and nums[lo]==nums[lo+1])lo++;
-                    while(lo+1<hi and nums[hi]==nums[hi-1])hi--;
+                    res.push_back({nums[i],nums[lo],nums[hi]});
+                    while(lo+1<hi and nums[lo]==nums[lo+1]) lo++;
+                    while(lo<hi-1 and nums[hi]==nums[hi-1]) hi--;
                     lo++;
                     hi--;
                 }
