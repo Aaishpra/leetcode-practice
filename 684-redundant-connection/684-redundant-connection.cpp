af8cad3482008e8be6e84abd576bb1f952779a46
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-03 15:52:43 +0530

    Time: 21 ms (21.85%), Space: 10.6 MB (13.03%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.cpp
+++ 684-redundant-connection/684-redundant-connection.cpp
@@ -1,48 +1,60 @@
-class UnionFind{
-    private:
-    vector<int> root;
-    vector<int> rank;
-    public:
-    UnionFind(int sz):root(sz),rank(sz){
-        for(int i=0;i<sz;i++){
-            root[i]=i;
-            rank[i]=1;
-        }
-    }
-    int find(int x){
-        if(x==root[x]) return x;
-        return root[x]=find(root[x]);
-    }
-    bool Union(int x,int y){
-        int rootx=find(x);
-        int rooty=find(y);
-        if(rootx!=rooty){
-            if(rank[rootx]>rank[rooty]){
-                root[rooty]=rootx;
-            }
-            else if(rank[rooty]>rank[rootx]){
-                root[rootx]=rooty;
-            }
-            else {
-                root[rooty]=rootx;
-                rank[rootx]+=1;
-            }
-            return true;
-        }
-        return false;
-    }
-    bool isconnected(int x,int y){
-        return find(x)==find(y);
-    }
-};
 class Solution {
 public:
+    int timer=0;
+    set<pair<int,int>> bridges;
+    void helper(vector<vector<int>>& arr,int n,vector<bool>& vis,vector<int>& tin,vector<int>& low,int node,int par){
+        vis[node]=true;
+        tin[node]=low[node]=timer;
+        ++timer;
+        
+        for(auto child:arr[node]){
+            if(child==par){
+                continue;
+            }
+            if(vis[child]){
+                low[node]=min(low[node],tin[child]);
+            }
+            else{
+                helper(arr,n,vis,tin,low,child,node);
+                low[node]=min(low[node],low[child]);
+                if(low[child]>tin[node]){
+                    bridges.insert({node,child});
+                }
+            }
+        }
+    }
+    
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
         int n=edges.size();
-        UnionFind uf(n+1);
+        vector<vector<int>> arr(n+1);
         for(auto e:edges){
-            if(uf.Union(e[0],e[1])==false) return e;
+            arr[e[0]].push_back(e[1]);
+            arr[e[1]].push_back(e[0]);
         }
-        return {};
+        
+        vector<bool> vis(n+1,false);
+        vector<int> tin(n+1,-1);
+        vector<int> low(n+1,-1);
+        
+        helper(arr,n,vis,tin,low,1,0);
+        
+        vector<int> isBridge(n,false);
+        for(int i=0;i<n;i++){
+            int e1=edges[i][0];
+            int e2=edges[i][1];
+            
+            if(bridges.find({e1,e2})!=bridges.end() || bridges.find({e2,e1})!=bridges.end()){
+                isBridge[i]=true;
+            }
+        }
+        vector<int> res;
+        for(int i=n-1;i>=0;i--){
+            if(!isBridge[i]){
+                res.push_back(edges[i][0]);    
+                res.push_back(edges[i][1]); 
+                break;
+            }
+        }
+        return res;
     }
 };
