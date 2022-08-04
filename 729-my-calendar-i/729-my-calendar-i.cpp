d591e76d55b892a51fe3148667448b53c7e6e023
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-04 07:17:34 +0530

    Time: 209 ms (31.70%), Space: 39.4 MB (17.93%) - LeetHub

--- 729-my-calendar-i/729-my-calendar-i.cpp
+++ 729-my-calendar-i/729-my-calendar-i.cpp
@@ -1,19 +1,17 @@
 class MyCalendar {
 public:
     vector<vector<int>> ar;
-    
     MyCalendar() {
         
     }
     
     bool book(int start, int end) {
+        
         for(int i=0;i<ar.size();i++){
-            if(!(ar[i][0]>=end or start>=ar[i][1])){
-                return false;
-            }
+            if(!(start>= ar[i][1] or end<=ar[i][0])) return false;
         }
-               ar.push_back({start,end});
-               return true;
+        ar.push_back({start,end});
+        return true;
     }
 };
 
