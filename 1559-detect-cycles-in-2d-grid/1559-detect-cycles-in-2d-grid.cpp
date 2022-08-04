6843dee55f40f1b759bdc750e1b1b5e5b16736a4
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-04 07:29:49 +0530

    Time: 453 ms (48.57%), Space: 60.9 MB (59.91%) - LeetHub

--- 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
+++ 1559-detect-cycles-in-2d-grid/1559-detect-cycles-in-2d-grid.cpp
@@ -1,24 +1,22 @@
 class Solution {
 public:
-    bool backtrack(vector<vector<char>>& ar,char target,int i,int j,vector<vector<int>> & vis,int n,int m){  
-        if(i<0 or j<0 or i>=n or j>=m or ar[i][j]!=target) return false;
+    bool solve(vector<vector<char>>& grid,vector<vector<int>>& vis,char target,int i,int j){
+        int n=grid.size(),m=grid[0].size();
+        if(i<0 or j<0 or j>=m or i>=n or grid[i][j]!=target) return false;
         if(vis[i][j]==1) return true;
         vis[i][j]=1;
-        ar[i][j]='#';
-        bool ans=backtrack(ar,target,i+1,j,vis,n,m) or backtrack(ar,target,i-1,j,vis,n,m) or backtrack(ar,target,i,j+1,vis,n,m) or backtrack(ar,target,i,j-1,vis,n,m);
-        ar[i][j]=target;
+        grid[i][j]='#';
+        bool ans=solve(grid,vis,target,i-1,j) or solve(grid,vis,target,i+1,j) or solve(grid,vis,target,i,j-1) or solve(grid,vis,target,i,j+1);
+        grid[i][j]=target;
         return ans;
-        
     }
     bool containsCycle(vector<vector<char>>& grid) {
-        int n=grid.size(), m=grid[0].size();
-    vector<vector<int>> vis(n,vector<int>(m,0));
-        
+        int n=grid.size(),m=grid[0].size();
+        vector<vector<int>> vis(n,vector<int>(m,0));
         for(int i=0;i<n;i++){
             for(int j=0;j<m;j++){
-                if(vis[i][j]==0 and backtrack(grid,grid[i][j],i,j,vis,n,m)){
+                if(vis[i][j]==0 and solve(grid,vis,grid[i][j],i,j))
                     return true;
-                }
             }
         }
         return false;
