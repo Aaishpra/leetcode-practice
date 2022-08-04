0aedbc91e01de88b01f53d18c0e1509c80b454d2
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-04 07:42:50 +0530

    Time: 37 ms (55.88%), Space: 15.1 MB (42.06%) - LeetHub

--- 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
+++ 332-reconstruct-itinerary/332-reconstruct-itinerary.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    void dfs(unordered_map<string,multiset<string>>& ar,string node,int dep,vector<string>& store,int tot){
-        while(ar[node].size()){
-            string child=*ar[node].begin();
-            ar[node].erase(ar[node].begin());
-            dfs(ar,child,dep+1,store,tot);
+    //Euclerian path
+    void dfs(unordered_map<string,multiset<string>>& graph,vector<string>& store,string start){
+        while(graph[start].size()>0){
+            auto next=*graph[start].begin();
+            graph[start].erase(graph[start].begin());
+            dfs(graph,store,next);
         }
-        store.push_back(node);
+        store.push_back(start);
     }
     vector<string> findItinerary(vector<vector<string>>& tickets) {
-        int n=tickets.size();
-        unordered_map<string,multiset<string>> ar;
-        vector<string> store;
+        unordered_map<string,multiset<string>> graph;
         for(auto t:tickets){
-            ar[t[0]].insert(t[1]);
+            graph[t[0]].insert(t[1]);
         }
-        
-        dfs(ar,"JFK",1,store,n);
+        vector<string> store;
+        dfs(graph,store,"JFK");
         reverse(store.begin(),store.end());
         return store;
+        
     }
 };
