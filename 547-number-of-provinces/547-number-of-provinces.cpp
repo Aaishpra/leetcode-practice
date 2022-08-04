5dbe36e531407a04f552e2da8416a648864ffc78
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-04 15:31:21 +0530

    Time: 14 ms (99.85%), Space: 13.8 MB (44.86%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,29 +1,78 @@
+class UnionFind {
+  private:
+    vector < int > root;
+    vector < int > rank;
+  public:
+    UnionFind(int sz): rank(sz), root(sz) {
+      for (int i = 0; i < sz; i++) {
+        root[i] = i;
+        rank[i] = 1;
+      }
+    }
+  int find1(int x) {
+    if (x == root[x])
+      return x;
+    return root[x] = find1(root[x]);
+  }
+
+  void unionSet(int x, int y) {
+    int root_x = find1(x);
+    int root_y = find1(y);
+    if (root_x != root_y) {
+      if (rank[root_x] > rank[root_y]) {
+        root[root_y] = root_x;
+      } else if (rank[root_x] < rank[root_y]) {
+        root[root_x] = root_y;
+      } else {
+        root[root_y] = root_x;
+        rank[root_x] += 1;
+      }
+      // return true;
+    }
+    //return false;
+  }
+  bool isConnected(int x, int y) {
+    return find1(x) == find1(y);
+  }
+
+};
 class Solution {
-public:
-    void dfs(vector<vector<int>>& grid,vector<int> &vis,int node){
-        vis[node]=1;
-        for(int i=0;i<grid.size();i++){
-           // if(i==node)continue;
-             if(vis[i]==0 and grid[node][i]){
-                dfs(grid,vis,i);
-            }
+  public:
+
+    int findCircleNum(vector < vector < int >> & grid) {
+      if (grid.empty()) return 0;
+
+      int m = grid.size();
+      int n = grid[0].size();
+      UnionFind uf(n + 1);
+      //int n=M.size();
+      vector < int > v(n, -1); //there will be maximum n group, mark all as a leader
+
+      //making group
+      for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+          if (grid[i][j] == 1)
+          {
+            uf.unionSet(i, j);
+          }
         }
-    }
-    int findCircleNum(vector<vector<int>>& grid) {
-        int n=grid.size();
-        vector<int> vis(n,0);
+      }
         
-        int count=0;
-        
-        for(int i=0;i<n;i++){
-            {
-                if(vis[i]==0){
-                    vis[i]=1;
-                    dfs(grid,vis,i);
-                    count++;
-                }
+      unordered_map < int, int > mp;
+      
+      int count = 0;
+      
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+          if (grid[i][j]==1) {
+            if (uf.find1(i) == uf.find1(j) and mp.find(uf.find1(i)) == mp.end()) {
+              count++;
+              mp[uf.find1(i)] = 1;
             }
+          }
         }
-        return count;
+      }
+      return count;
     }
+
 };
