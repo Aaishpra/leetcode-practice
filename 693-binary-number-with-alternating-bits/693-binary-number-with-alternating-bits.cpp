982cafe630c1a50f01ccfac2a3a05164f25d78ea
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-20 17:44:27 +0530

    Time: 4 ms (24.30%), Space: 6.2 MB (7.73%) - LeetHub

--- 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
+++ 693-binary-number-with-alternating-bits/693-binary-number-with-alternating-bits.cpp
@@ -5,7 +5,7 @@ public:
         vector<int> nums;
         while(n>0){
             nums.push_back(n%2);
-            n=n/2;
+            n=n>>1;
         }
         
         for(int i=1;i<nums.size();i++){
