9371feb41765b2c1aa2fb9e3fbd6df648e654fe8
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-29 20:33:49 +0530

    Time: 193 ms (77.69%), Space: 13.3 MB (12.32%) - LeetHub

--- 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
+++ 673-number-of-longest-increasing-subsequence/673-number-of-longest-increasing-subsequence.cpp
@@ -1,25 +1,33 @@
 class Solution {
 public:
     int findNumberOfLIS(vector<int>& nums) {
-      int n = nums.size(), maxlen = 1, ans = 0;
-        vector<int> cnt(n, 1), len(n, 1);
-        for (int i = 1; i < n; i++) {
-            for (int j = 0; j < i; j++) {
-                if (nums[i] > nums[j]) {
-                    if (len[j]+1 > len[i]) {
-                        len[i] = len[j]+1;
-                        cnt[i] = cnt[j];
+        vector<int> len(nums.size(),1);
+        vector<int> cnt(nums.size(),1);
+        int mx=0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<i;j++){
+                if(nums[i]>nums[j]){
+                    if(len[j]+1>len[i]){
+                        len[i]=len[j]+1;
+                        cnt[i]=cnt[j];
                     }
-                    else if (len[j]+1 == len[i]) 
-                        cnt[i] += cnt[j];
+                    else if(len[j]+1==len[i]){
+                      //  len[i]=len[j];
+                        cnt[i]+=cnt[j];
+                    }
                 }
             }
-            maxlen = max(maxlen, len[i]);
+            mx=max(len[i],mx);
         }
-        // find the longest increasing subsequence of the whole sequence
-       // sum valid counts
-        for (int i = 0; i < n; i++) 
-            if (len[i] == maxlen) ans += cnt[i];
-        return ans;   
+      int res=0;
+        
+        // for(int n:cnt) cout<<n<<" ";
+        // cout<<endl;
+        for(int i=0;i<nums.size();i++){
+            if(len[i]==mx){
+                res+=cnt[i];
+            }
+        }
+        return res;
     }
 };
