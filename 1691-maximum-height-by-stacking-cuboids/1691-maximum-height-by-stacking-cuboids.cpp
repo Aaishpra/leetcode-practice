1d8537d18786dce45483c8b018c4fde69bf7bfd6
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-31 20:26:52 +0530

    Time: 34 ms (30.35%), Space: 9.2 MB (44.89%) - LeetHub

--- 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
+++ 1691-maximum-height-by-stacking-cuboids/1691-maximum-height-by-stacking-cuboids.cpp
@@ -21,7 +21,7 @@ public:
             dp[i]=cuboids[i][2];
             for(int j=0;j<i;j++){
                 if(cuboids[i][0]>=cuboids[j][0] and cuboids[i][1]>=cuboids[j][1] and cuboids[i][2]>=cuboids[j][2]){
-                    dp[i]=max(dp[i],dp[j]+cuboids[i][2]);
+                    dp[i]=max(dp[i],dp[j]+max({cuboids[i][0],cuboids[i][1],cuboids[i][2]}));
                 }
             }
         }
