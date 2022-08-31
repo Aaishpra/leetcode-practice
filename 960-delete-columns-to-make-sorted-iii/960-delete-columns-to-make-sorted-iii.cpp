e237a2ff86f183dd3cd7c4301dcaec791cf98611
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-08-31 19:40:15 +0530

    Time: 23 ms (83.79%), Space: 10.3 MB (81.03%) - LeetHub

--- 960-delete-columns-to-make-sorted-iii/960-delete-columns-to-make-sorted-iii.cpp
+++ 960-delete-columns-to-make-sorted-iii/960-delete-columns-to-make-sorted-iii.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
-    int minDeletionSize(vector<string>& strs) {
-        int i,j, k;
-        int n=strs[0].size();
-        int m=strs.size();
-        vector<int>dp(n,1);
-        for(i=0;i<n;i++){
-            for(j=0;j<i;j++){
-                for(k=0;k<m;k++){
-                    if(strs[k][j]>strs[k][i]) break;
-                }
-                if(k==m&&dp[i]<1+dp[j]) dp[i]=1+dp[j];
-            }
-           
+    bool isLis(vector<string>& strs,int i,int j){
+        for(int itr=0;itr<strs.size();itr++){
+            if(strs[itr][j]>strs[itr][i])
+                return false;
         }
-         return n - *max_element(dp.begin(), dp.end());
-    
+        return true;
+    }
+    int minDeletionSize(vector<string>& strs) {
+        
+        int n=strs[0].size();
+        vector<int> dp(n,1);
+        for(int i=0;i<n;i++){
+            for(int j=0;j<i;j++){
+                if(isLis(strs,i,j)==true){
+                    dp[i]=max(dp[i],1+dp[j]);
+                }
+            }
+        }
+        return n-*max_element(dp.begin(),dp.end());
     }
 };
