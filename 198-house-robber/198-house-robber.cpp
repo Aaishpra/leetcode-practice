356b521e03fddf5a3e39feda330ae19b1ff7751f
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-09-01 22:21:01 +0530

    Time: 6 ms (17.19%), Space: 7.7 MB (77.30%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,20 +1,13 @@
 class Solution {
 public:
-    int solve(vector<int>& nums,int n,vector<int> &dp){
-        if(n<0)return 0;
-        // if(n==1) return nums[0];
-        // if(n==2) return max(nums[0],nums[1]);
-        if(dp[n]!=-1)return dp[n];
-        // int ans;
-        // for(int i=2;i<n;i++){
-        //         ans=
-        //     }
-        return dp[n]=max(nums[n]+solve(nums,n-2,dp),solve(nums,n-1,dp));
-    }
     int rob(vector<int>& nums) {
+        vector<int> dp(nums);
+        if(nums.size()==1)return nums[0];
         int n=nums.size();
-        vector<int> dp(nums.size()+1,-1);
-        return solve(nums,n-1,dp);
-        
+        dp[1]=max(nums[0],nums[1]);
+        for(int i=2;i<nums.size();i++){
+            dp[i]=max(nums[i]+dp[i-2],dp[i-1]);
+        }
+        return dp[n-1];
     }
 };
