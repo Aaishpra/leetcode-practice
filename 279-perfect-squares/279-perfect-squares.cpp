ee59b31d7f22bdeeb24e8bf7eafa178a3dc29e9a
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-09-01 23:38:39 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (99.81%) - LeetHub

--- 279-perfect-squares/279-perfect-squares.cpp
+++ 279-perfect-squares/279-perfect-squares.cpp
@@ -1,15 +1,22 @@
 class Solution {
 public:
+   
     int numSquares(int n) {
-       vector<int> dp(n+1);
-        dp[0]=0;
         
-        for(int i=1;i<=n;i++){
-            dp[i]=i;
-            for(int j=1;j*j<=i;j++){
-                dp[i]=min(dp[i],1+dp[i-j*j]);
-            }
+        //Legendre's 3 square theorem 
+        //All numbers can be represented as sum of 3 numbers 1,2,3 and only number in the form of 4^a(8b+7) is sum of 4 perfect squares
+        if(ceil(sqrt(n))==floor(sqrt(n))) return 1; // already Perfect square return 1;
+        while(n%4==0){
+            n=n/4;
         }
-        return dp[n];
+        if(n%8==7){
+            return 4; //In the form of 4^a(8b+7) then return 4;
+        }
+        for(int i=1;i*i<=n;i++){
+            int base= sqrt(n-i*i);
+            if(base*base==n-i*i) //if perfect square then return 2; 
+                return 2;
+        }
+        return 3; //all cases failed then return 3
     }
 };
