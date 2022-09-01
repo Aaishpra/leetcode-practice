361ff245a62b322ec86b64b93d0d2a2db2a083df
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-09-01 18:06:14 +0530

    Time: 3 ms (46.81%), Space: 6.4 MB (7.26%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.cpp
+++ 1025-divisor-game/1025-divisor-game.cpp
@@ -1,6 +1,17 @@
 class Solution {
 public:
+    int solve(int n,vector<int>& dp){
+        if(n==1) return dp[n]= 0;
+        if(dp[n]!=-1) return dp[n];
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                return dp[n]= !solve(n-i,dp);
+            }
+        }
+        return dp[n]=0;
+    }
     bool divisorGame(int n) {
-        return n%2==0;
+        vector<int> dp(n+1,-1);
+    return solve(n,dp);
     }
 };
