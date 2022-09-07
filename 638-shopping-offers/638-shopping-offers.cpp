04d8f8ad8f5cfa709ba294492b3dd51a04c0f402
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-09-08 00:33:16 +0530

    Time: 130 ms (25.12%), Space: 41.2 MB (10.18%) - LeetHub

--- 638-shopping-offers/638-shopping-offers.cpp
+++ 638-shopping-offers/638-shopping-offers.cpp
@@ -6,20 +6,20 @@ public:
         return solve(price,special,needs,0);
     }
     
-    int solve(vector<int>& price, vector<vector<int>>& special, vector<int> needs,int st){
-        if(st==special.size()){
+    int solve(vector<int>& price, vector<vector<int>>& special, vector<int> needs,int it){
+        if(it==special.size()){
             int cost=0;
             for(int i=0;i<needs.size();++i){
                 cost+=needs[i]*price[i];
             }
             return cost;
         }
-        if(mp.find({st,needs})!=mp.end()) return mp[{st,needs}];
+        if(mp.find({it,needs})!=mp.end()) return mp[{it,needs}];
         vector<int>need=needs;
         for(int j=0;j<price.size();++j){
-            need[j]=needs[j]-special[st][j];
-            if(need[j]<0) return mp[{st,needs}]=solve(price,special,needs,st+1);
+            need[j]=needs[j]-special[it][j];
+            if(need[j]<0) return mp[{it,needs}]=solve(price,special,needs,it+1);
         }
-        return mp[{st,needs}]=min(solve(price,special,needs,st+1),solve(price,special,need,st)+special[st][price.size()]);          // either choose it or don't choose
+        return mp[{it,needs}]=min(solve(price,special,needs,it+1),solve(price,special,need,it)+special[it][price.size()]);          // either choose it or don't choose
     }
 };
