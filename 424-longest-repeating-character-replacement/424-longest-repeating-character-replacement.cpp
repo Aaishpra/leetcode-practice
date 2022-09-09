c9228af60c0debfca61b58e5a9ea813a8281b27e
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-09-09 16:19:23 +0530

    Time: 31 ms (24.28%), Space: 7.3 MB (8.55%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int maxOnesIII(string& S, char& c, int& k) {
-       int s = 0, e = 0, t = 0, ans = 0, n = S.length();
-       while(e < n) {
-           while(e<n and (S[e] == c or t < k)) {
-               if(S[e] != c) t++;
-               e++;
-           }
-           
-           ans = max(ans, e-s);
-           while(s<e and S[s] == c) s++;
-           s++;
-           t--;
-       }
-      return ans;  
-    }
-    
-    int characterReplacement(string& s, int k) {
-      int chrCount[26] = {0};
-      int ans = 0;  
-      for(char& c : s) {
-          if(!chrCount[c-'A']) {
-              ans = max(ans, maxOnesIII(s, c, k));
-          } 
-          chrCount[c-'A'] = 1;
-      }
-      return ans;   
+    int characterReplacement(string s, int k) {
+        unordered_map<char,int> mp;
+        int i=0,j=0;
+        int res=0;
+        int mx=0;
+        while(j<s.size()){
+            mp[s[j]]++;
+            mx=max(mx,mp[s[j]]);
+            if(j-i+1-mx<=k){
+                res=max(res,j-i+1);
+                j++;
+            }
+            else{
+                mp[s[i]]--;
+                if(mp[s[i]]==0){
+                    mp.erase(s[i]);
+                }
+                i++;
+                j++;
+            }
+        }
+        return res;
     }
 };
