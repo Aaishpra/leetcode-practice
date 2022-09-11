3dfc2f3dffecf517cc8db2f0cb31e5df26d65b64
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-09-11 14:09:17 +0530

    Time: 175 ms (27.64%), Space: 36.8 MB (74.47%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -1,27 +1,28 @@
 class Solution {
 public:
+   // static bool comapre(const pair)
     int maxPerformance(int n, vector<int>& speed, vector<int>& eff, int k) {
     
         int mod=1e9+7;
-        priority_queue<pair<int,int>> pq;
-        
+        vector<pair<int,int>> pq;
         for(int i=0;i<speed.size();i++){
-            pq.push({eff[i],speed[i]});
+            pq.push_back({eff[i],speed[i]});
         }
+        sort(pq.rbegin(),pq.rend());
         long long res=0;
         long long sum=0;
         priority_queue<int,vector<int>,greater<int>> minheap;
         
-    while(!pq.empty()){
-            sum+=pq.top().second;
-            minheap.push(pq.top().second); 
+    for(int i=0;i<pq.size();i++){
+            sum+=pq[i].second;
+            minheap.push(pq[i].second); 
             if(minheap.size()>k){
                 sum-=minheap.top();
                 minheap.pop();
             }
             
-            res=max(res,pq.top().first*sum);
-            pq.pop();
+            res=max(res,pq[i].first*sum);
+            //pq.pop();
         }
         return res%mod;
     }
