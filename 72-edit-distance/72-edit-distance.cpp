4351fe036e77179dd91b844b787f3ced15e89e80
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-09-22 08:56:36 +0530

    Time: 165 ms (10.82%), Space: 79.8 MB (15.28%) - LeetHub

--- 72-edit-distance/72-edit-distance.cpp
+++ 72-edit-distance/72-edit-distance.cpp
@@ -1,28 +1,24 @@
 class Solution {
 public:
-    int minDistance(string word1, string word2) {
-     int n=word1.size(),m=word2.size();
-        int dp[n+1][m+1];
-        memset(dp,0,sizeof(dp));
-        
-        for(int i=1;i<n+1;i++){
-            dp[i][0]=i;
+    int dp[503][503];
+    
+    int solve(string x,string y,int n,int m){
+        if(n==0){
+            dp[n][m]=m;
         }
-        for(int j=1;j<m+1;j++){
-            dp[0][j]=j;
+        else if(m==0){
+            dp[n][m]=n;
         }
         
-        // 1+ dp[i-1][j-1] replace
-        // 1+ dp[i][j-1] insert
-        // 1+ dp[i-1][j] delete
-        for(int i=1;i<n+1;i++){
-            for(int j=1;j<m+1;j++){
-                if(word1[i-1]==word2[j-1]){
-                    dp[i][j]=dp[i-1][j-1];
-                }
-                else dp[i][j]=min(dp[i-1][j-1],min(dp[i-1][j],dp[i][j-1]))+1;
-            }
-        }
+        if(dp[n][m]!=-1)
+            return dp[n][m];
+        else if(x[n-1]==y[m-1])
+            dp[n][m]=solve(x,y,n-1,m-1);
+        else dp[n][m]=min(solve(x,y,n-1,m-1),min(solve(x,y,n-1,m),solve(x,y,n,m-1)))+1;
         return dp[n][m];
+    }
+    int minDistance(string word1, string word2) {
+        memset(dp,-1,sizeof dp);
+        return solve(word1,word2,word1.size(),word2.size());
     }
 };
