5e9df034eda66c6bf59ec5305900cea0716e3ab1
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-11-08 19:28:39 +0530

    Time: 4 ms (64.51%), Space: 6 MB (97.69%) - LeetHub

---
+++ 1544-make-the-string-great/1544-make-the-string-great.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string makeGood(string s) {
+        string res;
+        
+        for(int i=0;i<s.size();i++){
+        res.push_back(s[i]);
+            while(res.size()> 0 and( res.back()==s[i+1]-32 or res.back()==s[i+1]+32))
+                res.pop_back() ,i++;
+        }
+        return res;
+    }
+};
