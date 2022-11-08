8142b2392b7e374d315ade42c462324e43b0580a
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-11-08 22:56:14 +0530

    Time: 1480 ms (22.89%), Space: 194.4 MB (21.89%) - LeetHub

--- 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
+++ 1192-critical-connections-in-a-network/1192-critical-connections-in-a-network.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
-    int timer;
     vector<vector<int>> bridges;
-    void dfs(int node,int p,vector<vector<int>>& ar,vector<int>& vis,vector<int>& tin,vector<int>& low){
+    int timer;
+    void dfs(int node,int p,vector<vector<int>>& ar,vector<int> &vis,vector<int>& low,vector<int> &tin){
         vis[node]=1;
-        tin[node]=low[node]=timer++;
-        
-        for(auto child: ar[node]){
-        if(child==p) continue;
-            if(vis[child]==1){
-                low[node]=min(low[node],tin[child]);
-            }
-            else{
-                dfs(child,node,ar,vis,tin,low);
+        low[node]=tin[node]=timer++;
+        for(int child:ar[node]){
+            if(child==p) continue;
+            if(vis[child]==1) low[node]=min(low[node],tin[child]);
+            else {
+                dfs(child,node,ar,vis,low,tin);
                 low[node]=min(low[node],low[child]);
                 if(low[child]>tin[node])
                     bridges.push_back(vector<int>{node,child});
@@ -20,19 +17,20 @@ public:
         }
     }
     vector<vector<int>> criticalConnections(int n, vector<vector<int>>& connections) {
+        timer=0;
+        vector<vector<int>> ar(n);
         vector<int> vis(n,0);
         vector<int> tin(n,-1);
         vector<int> low(n,-1);
-        vector<vector<int>> ar(n);
-        timer=0;
-        for(auto& c:connections){
+        
+        for(auto c:connections){
             ar[c[0]].push_back(c[1]);
             ar[c[1]].push_back(c[0]);
         }
         
         for(int i=0;i<n;i++){
             if(vis[i]==0)
-                dfs(i,-1,ar,vis,tin,low);
+                dfs(i,-1,ar,vis,low,tin);
         }
         return bridges;
     }
