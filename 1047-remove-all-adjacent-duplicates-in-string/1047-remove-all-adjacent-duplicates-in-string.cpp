30dbab8e542a4669e783d3ee373930ee5b286287
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2022-11-10 09:19:11 +0530

    Time: 15 ms (97.00%), Space: 11 MB (66.38%) - LeetHub

---
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+       string res="";
+        
+        for(char c:s){
+            if(res.size()>0 and res.back()==c)
+                res.pop_back();
+            else res.push_back(c);
+        }
+        return res;
+    }
+};
