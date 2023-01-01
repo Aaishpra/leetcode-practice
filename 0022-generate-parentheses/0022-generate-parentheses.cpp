f4f9996b5c40eda35195f742beff75c0f3f1b8da
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2023-01-01 21:28:00 +0530

    Time: 3 ms (87.23%), Space: 15.9 MB (13.04%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    vector<string> res;
+    void solve(int open,int close,string op){
+        if(close==0 and open==0){
+            res.push_back(op);
+            return;
+        }
+        if(open>0){
+            string op1=op;
+            op1.push_back('(');
+            solve(open-1,close,op1);
+        }
+        if(close>open){
+            string op1=op;
+            op1.push_back(')');
+            solve(open,close-1,op1);
+        }
+    }
+    vector<string> generateParenthesis(int n) {
+        string op="";
+        solve(n,n,op);
+        return res;
+    }
+};
