b704339584a64895853dc0203612c9a87df5e399
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2023-01-10 23:36:18 +0530

    Time: 555 ms (44.07%), Space: 101.5 MB (84.50%) - LeetHub

--- 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
+++ 1870-minimum-speed-to-arrive-on-time/1870-minimum-speed-to-arrive-on-time.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    bool isValid(vector<int>& a,int mid,double hour){
-        double time = 0;
-    for (int i = 0; i < a.size(); i++) {
-        time = ceil(time);
-        time +=  (double)a[i] / mid;
-        if (time > hour) return false;
-    }
-    return true;
+    int isValid(vector<int>& dist,int mid,double hour){
+        int hr=0;
+        int n=dist.size();
+        double res=0;
+        for(int i=0;i<dist.size()-1;i++){
+            hr+=dist[i]/mid;
+            if(dist[i]%mid!=0) hr++;
+        }
+        res=hr+(double)dist[n-1]/mid;
+        return res<=hour;
     }
     int minSpeedOnTime(vector<int>& dist, double hour) {
-        int lo=1;
-        int hi=1e9;
-        int res=-1;
-        while(lo<=hi){
-            int mid=lo+(hi-lo)/2;
-            if(isValid(dist,mid,hour)){
-                res=mid;
-                hi=mid-1;
+        int lo=1,hi=1e9;
+            int res=-1;
+            while(lo<=hi){
+             int mid=lo+(hi-lo)/2;
+                if(isValid(dist,mid,hour)){
+                    res=mid;
+                    hi=mid-1;
+                }
+                else lo=mid+1;
             }
-            else lo=mid+1;
+            return res;
         }
-        return res;
-    }
+    
 };
