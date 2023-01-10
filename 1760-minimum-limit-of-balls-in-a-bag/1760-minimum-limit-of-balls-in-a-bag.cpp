75ec65f4c23f7b46e4cf742ebd55ff2df3fbb1e6
Shipra Verma <66299533+Aaishpra@users.noreply.github.com>
2023-01-10 16:04:15 +0530

    Time: 280 ms (65.30%), Space: 55.9 MB (79.43%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
-    bool isValid(vector<int>& nums,int m,int mid){
-        int x=0;
-        for(int n:nums){
-            x+=n/mid;
-            if(n%mid==0)x--;
+    bool isValid(vector<int>& nums,int k,int mid){
+        int cnt=0;
+        for(int i=0;i<nums.size();i++){
+            cnt+=nums[i]/mid;
+            if(nums[i]%mid==0) cnt--;
         }
-        return x<=m;
+        return cnt<=k;
     }
-    int minimumSize(vector<int>& nums, int maxOperations) {
-    int lo=1, hi=*max_element(nums.begin(),nums.end());
-        int res=0;
+    int minimumSize(vector<int>& nums, int k) {
+        int lo=1,hi=*max_element(nums.begin(),nums.end());
+        int res=-1;
         while(lo<=hi){
             int mid=lo+(hi-lo)/2;
-            if(isValid(nums,maxOperations,mid)){
+            if(isValid(nums,k,mid)){
                 res=mid;
                 hi=mid-1;
             }
